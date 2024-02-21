4f13ed33536696bbbc91eed4ad9fed688b9a0055
luisgufm <luisgu.faustino@gmail.com>
2024-02-21 15:15:03 -0300

    finalizei os exercicios sobre modulos

--- mundo01-python/Usando modulos em Python/desafio26.py
+++ mundo01-python/Usando modulos em Python/desafio26.py
@@ -1,4 +1,4 @@
-frase = input('Escreva uma frase bonita: ').strip()
-frase = frase.upper()
+frase = input('Escreva uma frase bonita: ').upper().strip()
 print(f'A letra A aparace {frase.count('A')}')
-print(frase.find('A'))
+print(f'A primeira letra A apareceu na posicao {frase.find('A')+1}')
+print(f'A ultima letra A apareceu na posicao {frase.rfind('A')+1}')
---
+++ mundo01-python/Usando modulos em Python/desafio27.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome completo: ').strip()
+dividido = nome.split()
+print(f'Muito prazer em te conhecer! {nome}')
+print(f'Seu primeiro nome e {dividido[0]}')
+print(f'Seu ultimo nome  e {dividido[len(dividido) - 1]}')
