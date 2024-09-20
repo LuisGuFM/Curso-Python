3e716c2e4522f3354a3b8a346a7194cc9c500d75
LuisGuFM <luisgu.faustino@gmail.com>
2024-09-20 00:23:16 -0300

    jaja eu termino

---
+++ mundo03-python/aulas/ex73.py
@@ -0,0 +1,13 @@
+times = ('Botafogo', 'Palmeiras', 'Fortaleza', 'Flamengo', 
+         'São Paulo', 'Bahia', 'Cruzeiro', 'Internacional', 
+         'Vasco', 'Atlético-MG', 'Juventude', 'Bragantino', 
+         'Atlético-PR', 'Grêmio', 'Criciúma', 'Fluminense', 
+         'Vitória', 'Corinthians', 'Cuiabá', 'Atlético-GO')
+print('-='*15)
+print(f'Lista de times: {times}')
+print('-='*15)
+print(f'Os primeiros times da tabela são: {times[0:5]}')
+print('-='*15)
+print(f'Os times na zona de rebaixamento são: {times[-4:]}')
+print('-='*15)
+print(f'Os times em ordem alfabética são: {sorted(times)}')
