d20b01fd003c7b06b875d5a064c4876aa3f1e9ad
LuisGuFM <luisgu.faustino@gmail.com>
2024-10-01 22:17:23 -0300

    listas em python

---
+++ mundo03-python/aulas/aula.py
@@ -0,0 +1,4 @@
+#LISTAS EM PYTHON
+#De inicio, para você montar uma lista em python, ultiliza-se o []
+#Para adicionar um item ao final da lista, use o lista.append((item))
+#Para adicionar um item na lista, em algum lugar em que voce deseja, use o lista.incert((posicao desejada), (item))
